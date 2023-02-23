6be8e280b2f546c48276bd781258dcbdc69d86a5
Stefany2 <1459565@senati.pe>
2023-02-23 11:36:48 -0500

    2da forma de sumar y mltiplicar

---
+++ Stefany.py
@@ -0,0 +1,10 @@
+#Suma y multiplicacion con dos 
+# variables o numeros
+
+n1=float(input("Ingrese un numero:"))
+n2=float(input("Ingrese un numero:"))
+suma=n1+n2
+multiplicacion=n1*n2
+print ("La suma es:",suma)
+print ("La multiplicacion es:",multiplicacion)
+
